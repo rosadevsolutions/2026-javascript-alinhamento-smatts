23c16c1ed8b6210bf1a2e813a4c083c976214f77
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2026-01-14 09:21:04 -0300

    etapa-01 exercicios-01 lista-01 correcao

--- ETAPA-01/EXERCICIOS-01/LISTA-01/app.js
+++ ETAPA-01/EXERCICIOS-01/LISTA-01/app.js
@@ -1,8 +1,8 @@
 /*
-01 - Neste diretório (bateria-de-exercicios-01), há um index.html vazio.
+01
+Neste diretório (bateria-de-exercicios-01), há um index.html vazio.
 
 Faça o seguinte:
-
 - Declare uma marcação HTML básica nele;
 - Modifique a linguagem para português do Brasil;
 - Remova as meta tags desnecessárias;
@@ -10,109 +10,113 @@ Faça o seguinte:
 - E linke este arquivo, app.js, antes do fechamento da tag body.
 */
 //Feito
-
+// ✔️ CERTO
 
 
 /*
-02 - Inicie o seu servidor local e abra o console do browser.
+02
+Inicie o seu servidor local e abra o console do browser.
 */
 // Feito
+// ✔️ CERTO
 
 
 
 /*
-03 - Para certificar-se que este arquivo realmente está linkado, exiba a frase
-"JS is Awesome!" em um alert no browser.
+03
+Para certificar-se que este arquivo realmente está linkado, exiba a frase "JS is Awesome!" em um alert no browser.
 
-Como mencionei durante a aula: não se preocupe em entender 100% o que os comandos
-"alert" e "console" fazem. Nós falaremos sobre eles mais à frente =)
+Como mencionei durante a aula: não se preocupe em entender 100% o que os comandos "alert" e "console" fazem. Nós falaremos sobre eles mais à frente =)
 
-Utilize os espaços entre os comentários abaixo para executar os códigos dos
-exercícios. Inclusive o alert mencionado acima.
+Utilize os espaços entre os comentários abaixo para executar os códigos dos exercícios. Inclusive o alert mencionado acima.
 */
 // Feito
 // alert("JS is Awesome!");
+// ✔️ CERTO
 
 
 
 /*
-04 - Comente o alert que você escreveu acima, utilizando o comentário de uma linha.
+04
+Comente o alert que você escreveu acima, utilizando o comentário de uma linha.
 */
 // Feito
+// ✔️ CERTO
 
 
 
 /*
-05 - Declare uma const "myBirthYear", que recebe o ano do seu nascimento, com 4
-dígitos. Ex.: 1930.
+05
+Declare uma const "myBirthYear", que recebe o ano do seu nascimento, com 4 dígitos. Ex.: 1930.
 */
 // Feito
 const myBirthYear = 1980;
+// ✔️ CERTO
 
 
 
 /*
-06 - Declare uma const "currentYear", que recebe o ano atual, com 4 dígitos.
+06
+Declare uma const "currentYear", que recebe o ano atual, com 4 dígitos.
 */
 // Feito
 const currentYear = 2026;
+// ✔️ CERTO
+
 
 
 /*
-07 - Exiba as const "myBirthYear" e "currentYear", lado a lado, no console do browser.
+07
+Exiba as const "myBirthYear" e "currentYear", lado a lado, no console do browser.
 */
 // Feito
 console.log(myBirthYear, currentYear)
+// ✔️ CERTO
+
+
 
 /*
-08 - Experimente.
+08
+Declare uma const "myAge", que recebe o ano atual subtraído pelo ano do seu nascimento, mas sem digitar os números manualmente.
 
-- Declare uma const "myAge", que recebe o ano atual subtraído pelo ano do seu
-nascimento. Mas sem digitar os números manualmente.
-
-Você pode utilizar o sinal de menos (-) entre os dois valores e verificar o que
-acontece.
+Você pode utilizar o sinal de menos (-) entre os dois valores e verificar o que acontece.
 
 - Exiba a "myAge" no console.
 */
 // Feito
 const myAge = currentYear - myBirthYear;
 //console.log(myAge)
+// ✔️ CERTO
 
 
 /*
-09 - Antes do console.log abaixo, declare uma const "ten", que recebe o número
-10 e:
-
+09
+Antes do console.log abaixo, declare uma const "ten", que recebe o número 10 e:
 - Descomente o console.log;
 - Substitua a palavra "valor1" pelo ano atual somado à 10;
 - Substitua a palavra "valor2" pela sua idade somada à 10;
 - Os números não devem ser digitados manualmente;
 - Observe o resultado no console do browser.
 
-Não se assuste com as crases e os cifrões. Conversaremos mais sobre operações
-matemáticas e strings (textos) em breve =)
+Não se assuste com as crases e os cifrões. Conversaremos mais sobre operações matemáticas e strings (textos) em breve =)
 */
-
 const ten = 10;
-console.log(`Em ${currentYear + 10}, estarei com ${myAge + 10} anos.`)
+console.log(`Em ${currentYear + ten}, estarei com ${myAge + ten} anos.`)
+// ✔️ CERTO
 
 /*
-
 10 - Abra o site abaixo no browser e faça o seguinte:
 
 http://whathappenedinmybirthyear.com/
 
 - Abra o console do devtools;
 - Provavelmente, haverão alguns warnings nele. Pressione ctrl + L para limpá-lo;
-- No console do devtools, declare uma const "myBirthYear" que recebe o ano em que
-você nasceu (4 dígitos) e pressione enter;
-- No final da 1ª linha do código abaixo, substitua SEU_ANO_DE_NASCIMENTO pelo uso
-da const myBirthYear;
-- Cole as duas linhas de código no console, pressione enter, feche o console e
-veja o que acontece.
+- No console do devtools, declare uma const "myBirthYear" que recebe o ano em que você nasceu (4 dígitos) e pressione enter;
+- No final da 1ª linha do código abaixo, substitua SEU_ANO_DE_NASCIMENTO pelo uso da const myBirthYear;
+- Cole as duas linhas de código no console, pressione enter, feche o console e veja o que acontece.
 
 document.querySelector('#birthYear').value = SEU_ANO_DE_NASCIMENTO
 document.querySelector('.formButton').click()
 */
 //O browser evibe uma contagem regressiva que decresce do ano atual até o ano definido como valor para myBithYear e passar a exibir os fatos importantes do ano definido na variavel myBithYear.
+// ✔️ CERTO
